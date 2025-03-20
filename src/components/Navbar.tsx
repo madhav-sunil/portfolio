e88df215a4dc6fa774c98039e9634e72efd1a93b
madhavsunil.ms@gmail.com <madhavsunil.ms@gmail.com>
2025-03-20 21:01:52 +0100

    feat: style nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,9 @@ export const Navbar: FC<NavbarProps> = ({ menuOpen, setMenuOpen }) => {
   }, [menuOpen]);
 
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
-      <div className="max-w-5xl mx-auto px-4">
-        <div className="flex justify-between items-center h-16">
+    <nav className="fixed  top-0 left-0 right-0 z-40">
+      <div className="max-w-5xl mx-auto px-4 justify-center">
+        <div className="flex justify-between items-center h-16 w-full bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
           <a href="#home" className="font-mono text-xl font-bold text-white">
             madhav<span className="text-blue-500">.tech</span>
           </a>
